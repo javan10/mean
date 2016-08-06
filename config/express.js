1023cdd850532eb975aaeb52416a5893c398f44a
javan <javan1000@gmail.com>
2016-08-06 02:44:16 -0300

    instalação do express-load pagina 27

--- config/express.js
+++ config/express.js
@@ -1,6 +1,8 @@
 var express = require('express');
 
-var home = require('../app/routes/home');
+//var home = require('../app/routes/home');
+
+var load = require('express-load');
 
 module.exports = function() {
 	var app = express();
@@ -8,6 +10,24 @@ module.exports = function() {
 	app.use(express.static('./public'));
 	app.set('view engine', 'ejs');
 	app.set('views', './app/views');
-	home(app);
+	//home(app);
+
+	/*
+	express-load : 
+		carregamento de nossos models,
+		controllers e routes, diminuindo 
+		drasticamente o uso da função require
+	*/
+	/*
+	O parâmetro {cwd: ‘app’} foi necessário para mudar
+	 diretório padrão, pois a função procura as pastas
+	 no diretório raiz contatooh e precisamos que ela
+	 considere a pasta contatooh/app
+	*/
+
+	load('models', {cwd:'app'}) 
+		.then('controllers')
+		.then('routes')
+		.into(app);
 	return app;
 };
