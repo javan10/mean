6bf7044b50a4f29b2495281c2e0fbde6bd6fb59a
javan <javan1000@gmail.com>
2016-08-06 02:24:06 -0300

    comite inicial

---
+++ app/routes/home.js
@@ -0,0 +1,8 @@
+// app/routes/home.js
+
+var controller = require('../controllers/home')();
+
+module.exports = function(app) {
+	app.get('/',controller.index);
+	app.get('/index',controller.index);
+}
